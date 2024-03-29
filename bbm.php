b8d89987b9d68c7e6458afa9842eeaf354d2c45d
abdurrahman <abdurrahman@smkwikrama.sch.id>
2024-03-29 11:00:52 +0700

    add-else-statement

--- bbm.php
+++ bbm.php
@@ -1,12 +1,9 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Shell</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
@@ -19,14 +16,12 @@
         }
     </style>
 </head>
-
 <body>
     <div class="container mt-5 pt-5">
         <div class="row">
             <div class="col-12 col-sm-8 col-md-6 m-auto">
                 <div class="card bg-dark text-white text-center">
                     <div class="card-body">
-
                         <form action="" method="POST" class="row d-flex align-items-center">
                             <div class="col">
                                 <input placeholder="masukan jumlah liter" type="number" name="liter" id="liter"
@@ -99,6 +94,11 @@ if (isset($_POST['submit']) && $_POST['liter'] && $_POST['jenis']) {
     $produk1 = new beli($liter, $jenis);
     $produk1->beli();
 
+} else {
+    echo '
+    <div class="alert alert-danger my-3" role="alert">
+        Silahkan Masukan Jumlah Liter dan Jenis Bensin
+    </div>';
 } //end if statement
 ?>
                         </div>
@@ -106,10 +106,6 @@ if (isset($_POST['submit']) && $_POST['liter'] && $_POST['jenis']) {
                 </div>
             </div>
         </div>
-
     </div>
-
-
 </body>
-
 </html>
